3cb3c75d9f68b225b55ee11b383d106492316ab7
Oliver <oliver.rottweil@gmail.com>
2019-01-07 13:31:09 +0100

    Larry

---
+++ src/Field.java
@@ -0,0 +1,53 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Field {
+    private Map<Integer, String> hashMap = new HashMap<>();
+
+        /*public static void main(String... args) {
+            Application application = new Application();
+            application.init();
+            application.filterByKey();
+            application.filterByValue();
+            application.filterByKeyAndValue();
+        }*/
+
+
+
+    public void filterByKey() {
+        System.out.println("--- filterByKey ---");
+
+        Map<Integer, String> result = hashMap.entrySet()
+                .stream()
+                .filter(map -> map.getKey() <= 22)
+                .collect(Collectors.toMap(map -> map.getKey(), map -> map.getValue()));
+
+        System.out.println("result : " + result);
+        System.out.println();
+    }
+
+    public void filterByValue() {
+        System.out.println("--- filterByValue ---");
+
+        Map<Integer, String> result = hashMap.entrySet()
+                .stream()
+                .filter(map -> "Orange".equals(map.getValue()))
+                .collect(Collectors.toMap(map -> map.getKey(), map -> map.getValue()));
+
+        System.out.println("result : " + result);
+        System.out.println();
+    }
+
+    public void filterByKeyAndValue() {
+        System.out.println("--- filterByKeyAndValue ---");
+
+        Map<Integer, String> result = hashMap.entrySet()
+                .stream()
+                .filter(p -> p.getKey() <= 22)                                           // filter by key
+                .filter(map -> map.getValue().startsWith("A"))                           // filter by value
+                .collect(Collectors.toMap(map -> map.getKey(), map -> map.getValue()));
+
+        System.out.println("result : " + result);
+    }
+}
---
+++ src/Wheat.java
@@ -0,0 +1,8 @@
+public class Wheat {
+     int CountGrain = 0;
+     int PosX = 0;
+     int PosY = 0;
+     public Wheat(int InPosX, int InPosY){
+
+     }
+}
