e2647b49e95e2ccc223a7f0d2169d565f9c4fcce
Oliver <oliver.rottweil@gmail.com>
2019-01-07 13:39:11 +0100

    Wheat Finished

--- src/Wheat.java
+++ src/Wheat.java
@@ -1,8 +1,10 @@
+import java.util.Random;
 public class Wheat {
-     int CountGrain = 0;
-     int PosX = 0;
-     int PosY = 0;
-     public Wheat(int InPosX, int InPosY){
-
+     Random rand = new Random();
+     int countGrain = 0;
+     int posX = 0;
+     int posY = 0;
+     public Wheat(int inPosX, int inPosY){
+         int grainCount = rand.nextInt(500) + 100;
      }
 }
